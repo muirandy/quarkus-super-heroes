4b7e39ca37e32a01d88db107f9321983c10fb247
Andy Muir <muirandy@yahoo.co.uk>
2025-10-07 23:12:18 +0100

    Up to end: Open API

--- super-heroes/rest-villains/src/test/java/io/quarkus/workshop/superheroes/villain/VillainResourceTest.java
+++ super-heroes/rest-villains/src/test/java/io/quarkus/workshop/superheroes/villain/VillainResourceTest.java
@@ -201,4 +201,9 @@ public class VillainResourceTest {
         };
     }
 
+    @Test
+    void shouldPingOpenAPI() {
+        given().header(ACCEPT, JSON).when().get("/q/openapi").then().statusCode(OK.getStatusCode());
+    }
+
 }
